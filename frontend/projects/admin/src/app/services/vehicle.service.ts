feac3af25c8d13cdb51752fd4d868ac977bab915
Thomas Manson <dev.mansonthomas@gmail.com>
2026-03-13 21:43:55 +0100

    fix(vehicle-service): use environment config and add credentials to API
    Agent-Id: agent-c5f1e91a-5724-403d-b127-cd71cf504d07

--- frontend/projects/admin/src/app/services/vehicle.service.ts
+++ frontend/projects/admin/src/app/services/vehicle.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Vehicle, VehicleUpdate, VehicleListResponse } from '../models/vehicle.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehicleService {
-  private readonly apiUrl = '/api/vehicles';
+  private readonly apiUrl = `${environment.apiUrl}/api/vehicles`;
 
   constructor(private http: HttpClient) {}
 
@@ -15,21 +16,21 @@ export class VehicleService {
    * Get list of vehicles filtered by user's UL
    */
   getVehicles(): Observable<VehicleListResponse> {
-    return this.http.get<VehicleListResponse>(this.apiUrl);
+    return this.http.get<VehicleListResponse>(this.apiUrl, { withCredentials: true });
   }
 
   /**
    * Get a specific vehicle by its synthetic name
    */
   getVehicle(nomSynthetique: string): Observable<Vehicle> {
-    return this.http.get<Vehicle>(`${this.apiUrl}/${nomSynthetique}`);
+    return this.http.get<Vehicle>(`${this.apiUrl}/${nomSynthetique}`, { withCredentials: true });
   }
 
   /**
    * Update vehicle metadata (calendar color, comments, etc.)
    */
   updateVehicle(nomSynthetique: string, update: VehicleUpdate): Observable<Vehicle> {
-    return this.http.patch<Vehicle>(`${this.apiUrl}/${nomSynthetique}`, update);
+    return this.http.patch<Vehicle>(`${this.apiUrl}/${nomSynthetique}`, update, { withCredentials: true });
   }
 }
 
